7eb6fff5d6c636e61b262c7653bd6754b84df6b5
jeonyul <jeonyul00@gmail.com>
2024-10-21 18:06:00 +0900

    feat: collectionView

--- PlanetPedia/MainViewController.swift
+++ PlanetPedia/MainViewController.swift
@@ -15,10 +15,15 @@ class MainViewController: UIViewController {
         super.viewDidLoad()
         planetCollectionView.dataSource = self
         planetCollectionView.delegate = self
+        planetCollectionView.showsVerticalScrollIndicator = false
     }
 
 
 }
+/*
+ UICollectionViewDelegate: 셀과 사용자가 상호작용할 때 (셀을 선택하거나 스크롤 등) 어떤 동작을 할지 설정.
+ UICollectionViewDelegateFlowLayout: 셀의 레이아웃, 즉 셀의 크기나 간격 등을 설정.
+ */
 
 extension MainViewController: UICollectionViewDataSource, UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -33,6 +38,16 @@ extension MainViewController: UICollectionViewDataSource, UICollectionViewDelega
         cell.planetNameLabel.text = target.englishName
         return cell
     }
-    
-    
+            
 }
+
+extension MainViewController:UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        guard let flowLayout = collectionViewLayout as? UICollectionViewFlowLayout else { return .zero }
+        
+        let width = Int((collectionView.bounds.width - (flowLayout.minimumInteritemSpacing + flowLayout.sectionInset.left + flowLayout.sectionInset.right)) / 2)
+        return CGSize(width: width, height: width)
+    }
+}
+
