4c73413c2972537520316c80bc353608047358c3
jeonyul <jeonyul00@gmail.com>
2024-10-21 16:17:07 +0900

    first commit

---
+++ PlanetPedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlanetPedia
+//
+//  Created by 전율 on 10/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
