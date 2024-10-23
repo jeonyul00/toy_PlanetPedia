58204dea62cbfbe2d79425c90c6cab77480331da
jeonyul <jeonyul00@gmail.com>
2024-10-23 21:40:35 +0900

    feat: compositionLayout

--- PlanetPedia/PlanetDetailViewController.swift
+++ PlanetPedia/PlanetDetailViewController.swift
@@ -10,9 +10,6 @@ import UIKit
 class PlanetDetailViewController: UIViewController {
     
     @IBOutlet weak var detailCollectionView: UICollectionView!
-            
-    
-    
     @IBOutlet weak var backgroundImageView: UIImageView!
     private let planet: Planet
     
@@ -30,9 +27,55 @@ class PlanetDetailViewController: UIViewController {
         detailCollectionView.dataSource = self
         let img = UIImage(named: "\(planet.englishName.lowercased())")
         backgroundImageView.image = img
+        setupLayout()
     }
     
+    func setupLayout() {
         
+        // 컴포지셔널 레이아웃 생성
+        let layout = UICollectionViewCompositionalLayout {  sectionIndex, environment in
+            switch sectionIndex {
+            case 1:
+                // item 크기 생성
+                var size = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .estimated(130))
+                // item 객체 생성
+                var item = NSCollectionLayoutItem(layoutSize: size)
+                size = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(130))
+                // 그룹 생성
+                var group = NSCollectionLayoutGroup.horizontal(layoutSize: size, subitems: [item])
+                // 그룹 내 여백
+                group.interItemSpacing = .flexible(20)
+                size = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(130))
+                item = NSCollectionLayoutItem(layoutSize: size)
+                
+                group = NSCollectionLayoutGroup.vertical(layoutSize: size, subitems: [group,item])
+                group.interItemSpacing = .fixed(20)
+                
+                // 섹션 생성
+                let section = NSCollectionLayoutSection(group: group)
+                // 섹션 여백
+                section.contentInsets = NSDirectionalEdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20)
+                section.interGroupSpacing = 20
+                return section
+            default:
+                // item 크기 생성
+                let size = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(200))
+                // item 객체 생성
+                let item = NSCollectionLayoutItem(layoutSize: size)
+                // 그룹 생성
+                let group = NSCollectionLayoutGroup.vertical(layoutSize: size, subitems: [item])
+                // 섹션 생성
+                let section = NSCollectionLayoutSection(group: group)
+                // 섹션 여백
+                section.contentInsets = NSDirectionalEdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20)
+                section.interGroupSpacing = 20
+                return section
+            }
+        }
+        // 컬렉션 뷰 레이아웃을 CompositionalLayout으로 변경
+        detailCollectionView.collectionViewLayout = layout
+    }
+            
 }
 
 extension PlanetDetailViewController:UICollectionViewDataSource {
